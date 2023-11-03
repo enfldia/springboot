73ee8d248dd3f3fa49a62a733486ba0a6db4888b
enfldia <enfldia12@naver.com>
2023-11-03 17:35:49 +0900

    1103

--- secondproject/src/main/resources/data.sql
+++ secondproject/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 -- article 더미데이터
-INSERT INTO article(id, nickname, title, content) VALUES(1, "111" '가가가가', '1111');
-INSERT INTO article(id, nickname, title, content) VALUES(2, "222" '나나나나', '2222');
-INSERT INTO article(id, nickname, title, content) VALUES(3, "333" '다다다다', '3333');
+INSERT INTO article(id, nickname, title, content) VALUES(1, '111' '가가가가', '1111');
+INSERT INTO article(id, nickname, title, content) VALUES(2, '222'' '나나나나', '2222');
+INSERT INTO article(id, nickname, title, content) VALUES(3, '333' '다다다다', '3333');
