f84c2a0244086ffb3c9f47384c1fd00b563cb562
enfldia <enfldia12@naver.com>
2023-11-21 09:32:50 +0900

    1121

--- shop/src/main/java/com/shop/dto/ItemFormDto.java
+++ shop/src/main/java/com/shop/dto/ItemFormDto.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.modelmapper.ModelMapper;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
