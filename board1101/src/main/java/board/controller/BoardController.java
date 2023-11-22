69404a2d8512f813dd970c2754ebdb65db3986a0
enfldia <enfldia12@naver.com>
2023-11-23 00:15:04 +0900

    1123

--- board1101/src/main/java/board/controller/BoardController.java
+++ board1101/src/main/java/board/controller/BoardController.java
@@ -3,8 +3,16 @@ package board.controller;
 import board.dto.BoardDTO;
 import board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -22,5 +30,58 @@ public class BoardController {
         System.out.println("boardDTO = " + boardDTO);
         boardService.save(boardDTO);
         return "index";
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model){
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable long id, Model model){
+        /*
+        해당 게시글의 조회수를 하나 올리고 게시글 데이터를 가져와서 detail.html에 출력
+         */
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "detail";
+    }
+
+    @GetMapping("update/{id}")
+    public String updateForm(@PathVariable Long id, Model model){
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("boardUpdate", boardDTO);
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute BoardDTO boardDTO,Model model){
+        BoardDTO board = boardService.update(boardDTO);
+        model.addAttribute("board", board);
+        return "detail";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        boardService.delete(id);
+        return "redirect:/board/";
+    }
+
+    @GetMapping("/paging")
+    public String paging(@PageableDefault(page = 1) Pageable pageable, Model model){
+        pageable.getPageNumber();
+        Page<BoardDTO> boardList =boardService.paging(pageable);
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        return "paging";
+
     }
 }
--- board1101/src/main/java/board/dto/BoardDTO.java
+++ board1101/src/main/java/board/dto/BoardDTO.java
@@ -1,5 +1,6 @@
 package board.dto;
 
+import board.entity.BoardEntity;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -17,7 +18,28 @@ public class BoardDTO {
     private String boardTitle;
     private String boardContents;
     private int boardHits; //조회수
-    private LocalDateTime boardCreateTime; //게시글 생성 시간
-    private LocalDateTime boardUpdateTime; //게시글 수정시 수정 시간
+    private LocalDateTime boardCreatedTime; //게시글 생성 시간
+    private LocalDateTime boardUpdatedTime; //게시글 수정시 수정 시간
 
+
+    public BoardDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
+        this.id = id;
+        this.boardWriter = boardWriter;
+        this.boardTitle = boardTitle;
+        this.boardHits = boardHits;
+        this.boardCreatedTime = boardCreatedTime;
+    }
+
+    public static BoardDTO toBoardDto(BoardEntity boardEntity){
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setId(boardEntity.getId());
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardPass(boardEntity.getBoardPass());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardHits(boardEntity.getBoardHits());
+        boardDTO.setBoardCreatedTime(boardEntity.getCreatedTime());
+        boardDTO.setBoardUpdatedTime(boardEntity.getUpdatedTime());
+        return boardDTO;
+    }
 }
